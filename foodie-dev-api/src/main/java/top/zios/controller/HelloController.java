7ddaa74416708d6debb997de2364c1c076114505
zios <756554206@qq.com>
2020-05-25 13:47:16 +0800

    first commit

---
+++ foodie-dev-api/src/main/java/top/zios/controller/HelloController.java
@@ -0,0 +1,14 @@
+package top.zios.controller;
+
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RestController;
+
+//@Controller
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Object hello(){
+        return "hello world";
+    }
+}
